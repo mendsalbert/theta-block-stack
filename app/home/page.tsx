d55085ff09a223980e6488dab4ab7abf88525227
mendsalbert <mendsalbert@gmail.com>
2024-06-26 11:07:28 +0000

    Subject: "Remove unused comment in homepage component"
    Body:
    "An extraneous comment line within the homepage component's code was eliminated. This clean-up contributes to maintaining the codebase's clarity and averts possible confusion for other developers in future. It is an essential step in conducting best-practice code hygiene."
    
    No issue reference is linked to this commit.

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -95,7 +95,6 @@ function Home() {
           </div>
         </div>
 
-        {/*  */}
         <div className="mt-8 gap-3 flex justify-center">
           <a
             className="inline-flex justify-center items-center gap-x-3 text-center bg-gradient-to-tl from-blue-600 to-violet-600 hover:from-violet-600 hover:to-blue-600 border border-transparent text-white text-sm font-medium rounded-md focus:outline-none focus:ring-1 focus:ring-gray-600 py-3 px-4 dark:focus:ring-offset-gray-800"
